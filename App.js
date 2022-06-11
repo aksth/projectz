78ced074b1de4dc8181e616ab9e57f5e803116fc
Akash Shrestha <shrestha.akash01@gmail.com>
2022-06-11 16:34:59 -0400

    search screen

--- App.js
+++ App.js
@@ -10,11 +10,12 @@ import { searchRecipes } from './api/spoonacular/recipes';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import { Ionicons } from '@expo/vector-icons'; 
+import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'; 
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/Settings';
 import { MyTheme } from './styles/theme';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import BrowseScreen from './screens/BrowseScreen';
 
 
 const Tab = createBottomTabNavigator();
@@ -37,16 +38,23 @@ export default function App() {
                     : 'home-outline';
                 } else if (route.name === 'Settings') {
                   iconName = focused ? 'settings' : 'settings-outline';
+                } else if (route.name === 'Browse') {
+                  iconName = focused ? 'text-box-search' : 'text-box-search-outline';
                 }
 
                 // You can return any component that you like here!
-                return <Ionicons name={iconName} size={size} color={color} />;
+                if(route.name === 'Browse') {
+                  return (<MaterialCommunityIcons name={iconName} size={size} color={color} />);
+                } else {
+                  return (<Ionicons name={iconName} size={size} color={color} />);
+                }
               },
               tabBarActiveTintColor: MyTheme.colors.primary,
               tabBarInactiveTintColor: MyTheme.colors.disabled,
             })}
         >
           <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Browse" component={BrowseScreen} />
           <Tab.Screen name="Settings" component={SettingsScreen} />
         </Tab.Navigator>
       </NavigationContainer>

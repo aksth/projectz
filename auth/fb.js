68a0b3d32f70e4562dbf9059ccdaa545d10d292e
Akash Shrestha <shrestha.akash01@gmail.com>
2022-06-13 01:25:51 -0400

    facebook login

--- auth/fb.js
+++ auth/fb.js
@@ -6,51 +6,63 @@ import {
   signOut,
 } from 'firebase/auth';
 import * as Facebook from 'expo-facebook';
+import { removeFromStore, saveToStore } from '../storage/store';
 
-const auth = getAuth();
-
-// Listen for authentication state to change.
+/* // Listen for authentication state to change.
 onAuthStateChanged(auth, user => {
-  console.log('auth state changed!')
   if (user != null) {
     console.log('We are authenticated now!');
-    console.log(user);
+    //console.log(user);
   }
-
   // Do other things
-});
+}); */
 
-async function loginWithFacebook() {
-  await Facebook.initializeAsync('628756465508369');
+async function loginWithFacebook(callback) {
+
+  const auth = getAuth();
+
+  await Facebook.initializeAsync({appId: '628756465508369', appName:'projectz'});
 
   const { type, token } = await Facebook.logInWithReadPermissionsAsync({
-    permissions: ['public_profile'],
+    permissions: ['public_profile', 'email'],
   });
 
   if (type === 'success') {
 
     console.log("####### faceook app login success ########");
-    console.log(token);
+    //console.log(token);
 
     // Build Firebase credential with the Facebook access token.
     // const facebookAuthProvider = new FacebookAuthProvider();
     const credential = FacebookAuthProvider.credential(token);
 
     // Sign in with credential from the Facebook user.
-    signInWithCredential(auth, credential).catch(error => {
+    const userCredential = await signInWithCredential(auth, credential).catch(error => {
       console.log(error);
       // Handle Errors here.
     });
+    console.log("####### firebase facebook auth login success ########");
+    //console.log(userCredential);
+    await saveToStore('email', userCredential._tokenResponse.email);
+    await saveToStore('loggedIn', 'true');
+    await saveToStore('idToken', userCredential._tokenResponse.idToken);
+    await saveToStore('fullName', userCredential._tokenResponse.fullName);
+    callback();
   } else {
     console.log('facebook login error.');
   }
 }
 
-function signOutFacebook() {
+async function signOutFacebook(callback) {
   const auth = getAuth();
   signOut(auth).then(() => {
     // Sign-out successful.
     console.log('sign out successful.');
+    removeFromStore('email');
+    removeFromStore('loggedIn');
+    removeFromStore('idToken');
+    removeFromStore('fullName');
+    callback();
   }).catch((error) => {
     // An error happened.
     console.log('sign out error.');
